03fd41fac029d6e6b2318bf14f3e09200a23506b
arthurM <arthur.martep@gmail.com>
2022-06-24 21:51:48 -0300

    Criação de usuario praticamente pronta

--- cliente.py
+++ cliente.py
@@ -1,14 +1,14 @@
-from os import system
+import sqlite3
 import socket
+import os
 import json
+import sys
 import threading
+
 
 HOST = '127.0.0.1'
 PORT = 50000   # cliente só se conecta com o servidor se a porta for a mesma
 
-
-# jsonResult = {"first": "You're", "second": "Awsome!"}
-# jsonResult = json.dumps(jsonResult)
 
 def tela_inicio():
     print('\n--------------------------------------')
@@ -17,35 +17,51 @@ def tela_inicio():
     print('1: Fazer Cadastro\n''2: Fazer Login\n''3: Sair')
 
 
-def switch(num, client):
+def tela_sistema():
+    print('\n--------------------------------------')
+    print('Olá, você está logado no sistema CarnaTroca.')
+    print('--------------------------------------')
+    print('1: Anunciar itens para troca\n''2: Listar todos os itens disponíveis para troca')
+    print('3: Ver meus anúncios\n')
+
+
+def switch_tela_inicio(num, client):
     if num == '1':
         print('\nBora fazer o cadastro')
-        nome = input('\nEscolha seu nome de usuario:')
-        senha = input('Escolha uma senha para sua conta:')
-        user = {nome, senha}  # TENHO QUE APRENDER A PASSAR ISSO AQUI
-        client.sendall(nome.encode('utf-8'))
+        user = {
+            'nome': input('\nEscolha seu nome de usuario:'),
+            'senha': input('Escolha uma senha para sua conta:')
+        }
+
+        envio = json.dumps(user)
+        # envia a mensagem para o servidor
+        client.sendall(envio.encode('utf-8'))
         data = client.recv(1024)
 
         print('---------------------------------------------')
         if (data.decode('utf-8') == 'Aprovado!'):         # decodifica o dado para conseguir ler
-            print(f'A resposta do servidor foi:', {data.decode('utf-8')})
+            # tela_sistema()
+            # os.system('clear')
+            print('Cadastro realizado com sucesso!!')
+            control = input('Escolha uma das opcoes acima para continuar:')
+            tela_sistema(control, client)
         else:
-            print(data.decode())
             print('Este nome de usuario já esta cadastradado!')
             print('Tente utilizar outro nome de usuario.')
             tela_inicio()
             num = input('Escolha uma das opcoes acima para continuar:')
-            switch(num, client)
+            switch_tela_inicio(num, client)
 
     elif num == '2':
         print('Bora fazer login')
     elif num == '3':
-        print('Vamos sair!')
+        print('Você foi desconectado do sistema. :/')
+        client.close()
     else:
-        print('\nOpcao inválida.\nEscolha uma opção válida!\n')
+        print('\nOpcao inválida.\nEscolha uma opção válida!')
         tela_inicio()
         num = input('Escolha uma das opcoes acima para continuar:')
-        switch(num, client)
+        switch_tela_inicio(num, client)
 
 
 def main():
@@ -54,26 +70,35 @@ def main():
         client.connect((HOST, PORT))  # tenta se conectar ao servidor
     except Exception as e:
         # se não conseguir mostra esta mensagem de erro
-        return print('\nNão foi possívvel se conectar ao servidor!\n')
+        return print('\nNão foi possível se conectar ao servidor!\n')
 
     tela_inicio()
     num = input('Escolha uma das opcoes acima para continuar:')
-    switch(num, client)
+    switch_tela_inicio(num, client)
 
-    """while True:
-        # system('clear')
-        msg = input('O que devemos enviar para o servidor?: ')
-        client.sendall(msg.encode('utf-8'))
-        if msg == 'sair':
-            print('Client está se desconectando!')
-            break
-
-        # quando o servidor responde, grava os dados na variavel data
-        data = client.recv(1024)
-        # decodifica o dado para conseguir ler
-        print(f'A resposta do servidor foi:', {data.decode()})"""
-
+    print('ENTREI AQUI NO CLOSE')
     client.close()
 
 
 main()
+
+
+
+
+""""
+def switch_tela_sistema(control, client):
+    if control == '1':
+        print('Anunciar itens para troca!')
+    elif control == '2':
+        print('Ver todos os itens do sistema!')
+    elif control == '3':
+        print('Ver todos meus anuncios')
+    elif control == '4':
+        print('Você foi desconectado do sistema. :/')
+        client.close()
+    else:
+        print('\nOpcao inválida.\nEscolha uma opção válida!')
+        tela_sistema()
+        num = input('Escolha uma das opcoes acima para continuar:')
+        switch_tela_sistema(control, client)
+"""
--- servidor.py
+++ servidor.py
@@ -1,5 +1,7 @@
 import socket
 import threading
+import sqlite3
+import json
 
 
 HOST = 'localhost'
@@ -13,25 +15,51 @@ print('Aguardando conexao de um cliente...')
 
 
 def novo_client(client, connection):
+
     ip = connection[0]
     port = connection[1]
     print(f'Novo cliente conectado em IP:{ip}, e porta:{port}!')
+
+    with sqlite3.connect('carna_troca.db') as db:
+        while True:
+            msg = client.recv(1024)  # recebe a mensagem enviada pelo cliente
+            print(msg.decode())
+            aux = msg.decode().split('"')  # recebe a mensagem e separa em strings onde tem "
+            nome = aux[3] # pega apenas a string que corresponde ao nome de usuario 
+            senha = aux[7]  # pega apenas a string que corresponde à senha
+            print(nome)
+            print(senha)
+
+            cursor = db.cursor()  # possibilita fazer as operações com o banco
+
+            #faz a verificação no banco de dados
+            cursor.execute(f"SELECT user_name FROM user WHERE user_name = '{nome}'")
+            verifica = str(cursor.fetchall())  # verifica recebe o conteúdo do select
+            if (verifica) != '[]':  # se for != de vazio, signifca que tem dado na tabela
+                verifica = verifica.split("'")
+                verifica = verifica[1]
+                if verifica == nome:
+                    reply = ('erro')
+                    client.sendall(reply.encode('utf-8'))
+            else:
+                # se o nome de usuario for diferente
+                print(f'O cliente da porta {port}, disse: {msg.decode()}')
+                reply = (f'Aprovado!')
+                # insere os dados no banco
+                cursor.execute(f"INSERT INTO user VALUES ('{nome}','{senha}')")
+                db.commit()
+                client.sendall(reply.encode('utf-8'))
+
+
+def threads():
     while True:
-        msg = client.recv(1024)  # recebe a mensagem enviada pelo cliente
-        if msg.decode() == 'sair':
-            break
-        print(f'O cliente da porta {port}, disse: {msg.decode()}')
-        reply = (f'Aprovado!')
-        client.sendall(reply.encode('utf-8'))
-    print(f'O cliente do IP:{ip}, e porta:{port}!, foi desconectado!')
-    client.close()
+        try:
+            client, ip = server.accept()  # server.accept (método para aceitar a conexão)
+            thread1 = threading.Thread(target=novo_client, args=[client, ip])
+            thread1.start()
+        except KeyboardInterrupt:
+            print(f'Desligando o servidor!')
+            server.close()
 
 
-while True:
-    try:
-        client, ip = server.accept()  # server.accept (método para aceitar a conexão)
-        thread1 = threading.Thread(target=novo_client, args=[client, ip])
-        thread1.start()
-    except KeyboardInterrupt:
-        print(f'Desligando o servidor!')
-        server.close()
+threads()
